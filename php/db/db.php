91ca9bb8c2dc30370e9263eaa1c9a47b670511da
goudjanoueddie <goudjanoueddie@gmail.com>
2021-03-06 13:25:28 +0000

    db php modified

--- php/db/db.php
+++ php/db/db.php
@@ -1,20 +1,8 @@
 <?php 
 $server = "127.0.0.1";
-<<<<<<< HEAD
-<<<<<<< HEAD
 $user="root";
 $pass="Root123@";
 $dbName="sapressiDB";
-=======
-$user = "root";
-$pass = "rootroot";
-$dbName = "sapressiDB";
->>>>>>> c5fb6026cc1a08dd1af1d1fa8c1c67b5665553eb
-=======
-$user = "root";
-$pass = "rootroot";
-$dbName = "sapressiDB";
->>>>>>> cafc04e11262dfb2840e927a94977a6ead55ef09
 
 $mysqli = new mysqli($server,$user,$pass,$dbName);
 
@@ -23,5 +11,4 @@ $mysqli = new mysqli($server,$user,$pass,$dbName);
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
     }
-
 ?>
